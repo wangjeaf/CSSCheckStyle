16c1bc1691acae7e1d935cf0590d357f741599a9
wangjeaf <wangjeaf@gmail.com>
2012-11-07 11:55:39 +0800

    add experiment dir in test

---
+++ tests/experiment/test.py
@@ -0,0 +1,19 @@
+import sys
+from ckstyle.CssCheckerWrapper import doCheck
+from ckstyle.reporter.ReporterUtil import ReporterUtil
+from ckstyle.cssparser.CssFileParser import CssParser
+from ckstyle.entity.StyleSheet import StyleSheet
+
+def checkCssFileByOpm(filePath):
+    fileContent = open(filePath).read()
+    checker = doCheck(fileContent, filePath)
+    if checker.hasError():
+        reporter = ReporterUtil.getReporter('text', checker)
+        reporter.doReport()
+        print reporter.export()
+        return False
+    return True
+
+
+if __name__ == '__main__':
+    checkCssFileByOpm('test.css')
