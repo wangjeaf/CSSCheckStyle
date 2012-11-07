f51779d38caf926b744c9fbe5ff370ae8cd63389
wangjeaf <wangjeaf@gmail.com>
2012-11-07 11:43:18 +0800

    add unit tests

---
+++ tests/unit/helper.py
@@ -0,0 +1,10 @@
+from ckstyle.CssCheckerWrapper import doCheck
+from ckstyle.reporter.ReporterUtil import ReporterUtil
+
+def doCssCheck(fileContent, level = 2):
+    checker = doCheck(fileContent)
+    return checker.errors()
+
+def doCssTextCheck(text, fileName = ''):
+    checker = doCheck(text, fileName)
+    return checker.errors()
