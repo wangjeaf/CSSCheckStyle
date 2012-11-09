337f6d67bce331a26b44e65671c63f223f0c5ebc
wangjeaf <wangjeaf@gmail.com>
2012-11-09 11:19:26 +0800

    add help

--- ckstyle/command/ConsoleCommandParser.py
+++ ckstyle/command/ConsoleCommandParser.py
@@ -1,3 +1,6 @@
+#/usr/bin/python
+#encoding=utf-8
+
 import sys
 import os
 import getopt
@@ -9,13 +12,26 @@ def usage():
     print '''
 [Usage]
     ckstyle -h / ckstyle --help
+    ckstyle
     ckstyle file.css
     ckstyle dir 
     ckstyle -r dir
-    ckstyle -p file.css
     ckstyle -p -r dir
-    ckstyle -c config_file_path
-    ckstyle -c config_file_path -r -p
+    ckstyle -c xxx.ini 
+    ckstyle -c xxx.ini -r -p
+
+[Example]
+    ckstyle -c xxx.ini -r -p -c xxx.ini --extension=.test.txt --include=all --exclude=none --errorLevel=2 dirpath
+
+[Options]
+    -h / --help     show help
+    -r              check files in directory recursively
+    -p              print check result to console(delete result files at the same time)
+    -c / --config   specify the config file name(use "~/ckstyle.ini" as default)
+    --include       specify rules(can be configed in .ini file)
+    --exclude       specify exclude rules(can be configed in .ini file)
+    --extension     specify check result file extension(use ".ckstyle.txt" as default)
+    --errorLevel    specify error level(0-error, 1-warning, 2-log)
     '''
 
 def getDefaultConfigPath():
