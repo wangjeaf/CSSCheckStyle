11ac8f69f546e1b93e0216df8c2026f443e06aaa
wangjeaf <wangjeaf@gmail.com>
2012-11-22 19:33:51 +0800

    add extra test

--- tests/unit/compress/CompressFile.py
+++ tests/unit/compress/CompressFile.py
@@ -3,6 +3,7 @@ from helper import *
 def doTest():
     _basic()
     _one_line_file()
+    _with_extra()
 
 def _basic():
     msg = doCssFileCompress('_file.css')
@@ -11,3 +12,8 @@ def _basic():
 def _one_line_file():
     msg = doCssFileCompress('_one_line_file.css')
     equal(msg, ".test{width:100px;height:200px;_z-index:111}@keyframes 'name'{10% {width: 100px;}}.another{*width:100px;color:#DDD;background-color:#ABC}", 'file compressed')
+
+def _with_extra():
+    msg = doCssFileCompress('_with_extra.css')
+    equal(msg, "@charset utf-8;@import url('xxxxx');@namespace lalala;.test{width:100px;height:200px;_z-index:111}@import url('xxx2');@import url('xxx3');", 'file compressed')
+    
---
+++ tests/unit/todos/TryParseKeyFrames.py
@@ -0,0 +1,4 @@
+from helper import *
+
+def doTest():
+    pass
