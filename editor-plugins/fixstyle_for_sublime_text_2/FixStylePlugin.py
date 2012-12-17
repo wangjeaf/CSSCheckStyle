77e25653a5460be8e2c1f633113f0d13856ce7b1
wangjeaf <wangjeaf@gmail.com>
2012-12-17 13:13:39 +0800

    add readme for sublime-text

---
+++ editor-plugins/fixstyle_for_sublime_text_2/FixStylePlugin.py
@@ -0,0 +1,19 @@
+#encoding=utf-8
+import sublime, sublime_plugin
+import os
+
+class FixstyleCommand(sublime_plugin.TextCommand):
+
+    def run(self, edit):
+        path = os.path.realpath(self.view.file_name()).encode('utf-8')
+        cmd = 'fixstyle -p "' + path + '"'
+        returnValue = os.popen3(cmd)
+
+        returnValue = returnValue[1].read() + returnValue[2].read()
+
+        if returnValue.find('[console.error]') != -1:
+            sublime.error_message(returnValue)
+        else:
+            region = sublime.Region(0, self.view.size())
+            self.view.replace(edit, region, returnValue)
+            self.view.end_edit(edit)
---
+++ editor-plugins/fixstyle_for_sublime_text_2/README.md
@@ -0,0 +1,18 @@
+##ckstyle plugin for Sublime Text 2
+
+### 安装说明
+* 找到Sublime Text 2的插件目录，例如：`C:\Documents and Settings\Administrator\Application Data\Sublime Text 2\Packages`
+* 将此zip内的内容解压缩到插件目录下即可
+
+### 命令说明
+
+`Fixstyle` 为样式修复
+`CssCompress` 为代码压缩
+
+### 使用方法
+
+* Fixstyle快捷键：`alt + tab` ，CssCompress无快捷键
+
+* 菜单项：`tools->Fixstyle`   `tools->CssCompress`
+
+* 右键菜单 `Fixstyle`  `CssCompress`
